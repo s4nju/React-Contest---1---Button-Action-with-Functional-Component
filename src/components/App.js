6d93fb27f451c6ab15249ee1e2787c838a7d442e
Sanjib Kumar Sah <35219515+s4nju@users.noreply.github.com>
2020-10-21 21:40:26 +0530

    added button and para

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [buttonClicked, setButtonClicked] = React.useState(false);
+  function handleClick() {
+    let buttonClickedCopy = buttonClicked;
+    buttonClickedCopy = !buttonClickedCopy;
+
+    setButtonClicked(buttonClickedCopy);
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* Do not alter the main div */}
+      <button id="click" onClick={() => handleClick()}></button>
+      {buttonClicked ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
